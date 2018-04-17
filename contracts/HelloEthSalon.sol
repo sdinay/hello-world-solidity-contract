8d7bb1e794ea1c5af4f58a3ed984ff25b9b4001a
sdinay <sdinay@yahoo.com>
2018-04-17 14:05:21 -0700

    following hello-ethereum-solan-contract tutorial

---
+++ contracts/HelloEthSalon.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.4.4;
+
+contract HelloEthSalon {
+  string message = "Hello Ethereum Salon!";
+
+  function HelloEthSalon() {
+    // constructor
+  }
+
+  function GetMessage() returns (string) {
+    return message;
+  }
+
+}
